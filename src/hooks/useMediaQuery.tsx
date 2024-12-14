33421f701438d66abdf9efe077ad28e72245080c
Major-Chuks <chukaezeka@gmail.com>
2024-12-14 23:02:23 +0100

    test

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -5,6 +5,8 @@ interface ScreenSize {
   height: number;
 }
 
+// test commit
+
 const useMediaQuery = (): ScreenSize => {
   const [screenSize, setScreenSize] = useState<ScreenSize>({
     width: 0,
