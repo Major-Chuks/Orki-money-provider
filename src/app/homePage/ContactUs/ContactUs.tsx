ed155c99546dae274a4c45768aa274a189a4f7c4
Major-Chuks <chukaezeka@gmail.com>
2025-04-14 08:59:03 +0100

    update app animation

--- src/app/homePage/ContactUs/ContactUs.tsx
+++ src/app/homePage/ContactUs/ContactUs.tsx
@@ -1,6 +1,6 @@
 "use client";
 import classes from "./ContactUs.module.css";
-import bg from "@/assets/contactUs-bg.png";
+// import bg from "@/assets/contactUs-bg.png";
 import coin1 from "@/assets/coin-1.svg";
 import coin2 from "@/assets/coin-2.svg";
 import coin3 from "@/assets/coin-3.svg";
@@ -9,6 +9,8 @@ import Responsive from "@/components/Responsive/Responsive";
 import Image from "next/image";
 import CustomButton from "@/components/CustomInput/CustomButton/CustomButton";
 import SlideUp from "@/components/SlideUp/SlideUp";
+import GlowingDotGrid from "@/components/GlowingDotGrid/GlowingDotGrid";
+import useWidth from "@/hooks/useWidth";
 
 const ContactUs = ({
   title,
@@ -22,10 +24,12 @@ const ContactUs = ({
     description ||
     " Unlock Crypto Success: Your Shortcut to Smarter and Refined Transactions with us.";
 
+  const width = useWidth();
+
   return (
     <Responsive style={{ maxWidth: "1312px" }}>
       <div
-        style={{ backgroundImage: `url(${bg.src})` }}
+        // style={{ backgroundImage: `url(${bg.src})` }}
         className={classes.container}
       >
         <div className={classes.heading}>
@@ -36,11 +40,20 @@ const ContactUs = ({
             <SlideUp>{_description}</SlideUp>
           </div>
         </div>
-        <SlideUp>
-          <CustomButton style={{ width: "max-content", alignSelf: "center" }}>
-            Contact Us
-          </CustomButton>
-        </SlideUp>
+        <div className={classes.btnContainer}>
+          <SlideUp>
+            <CustomButton
+              style={{
+                width: "max-content",
+                alignSelf: "center",
+                position: "relative",
+                zIndex: "9",
+              }}
+            >
+              Contact Us
+            </CustomButton>
+          </SlideUp>
+        </div>
         <Image
           className={`${classes.coin} ${classes.coin1} `}
           src={coin1}
@@ -61,6 +74,32 @@ const ContactUs = ({
           src={coin4}
           alt=""
         />
+
+        <div className={classes.animatedBox}>
+          {width > 768 ? (
+            <GlowingDotGrid
+              width={1180}
+              height={580}
+              rows={8}
+              cols={16}
+              dotSize={8}
+              borderRadius={1000}
+              dotColor="#1B7956"
+              strokeColor="rgb(36, 124, 91, 0.2)"
+            />
+          ) : (
+            <GlowingDotGrid
+              width={440}
+              height={580}
+              rows={12}
+              cols={12}
+              dotSize={4}
+              borderRadius={1000}
+              dotColor="#1B7956"
+              strokeColor="rgb(36, 124, 91, 0.2)"
+            />
+          )}
+        </div>
       </div>
     </Responsive>
   );
