942011f2f9ed6b6289586f9e726ca10fb7008da5
Major-chuks <chukaezeka@gmail.com>
2025-07-30 13:19:03 +0100

    debug websocket

--- src/hooks/useEcho.tsx
+++ src/hooks/useEcho.tsx
@@ -19,17 +19,17 @@ export function useEcho() {
       window.Pusher = Pusher;
     }
 
-    const isLocalhost =
-      window.location.hostname === "localhost" ||
-      window.location.hostname === "127.0.0.1" ||
-      window.location.hostname === "[::1]";
+    // const isLocalhost =
+    //   window.location.hostname === "localhost" ||
+    //   window.location.hostname === "127.0.0.1" ||
+    //   window.location.hostname === "[::1]";
 
     // Initialize Echo
     const echoInstance = new Echo({
       broadcaster: "reverb",
       key: process.env.NEXT_PUBLIC_WEBSOCKET_ID!,
       wsHost: "ws.money.orki.io",
-      forceTLS: !isLocalhost,
+      forceTLS: false,
       enabledTransports: ["ws", "wss"],
       wsPort: 80,
       wssPort: 80,
@@ -39,7 +39,7 @@ export function useEcho() {
       broadcaster: "reverb",
       key: process.env.NEXT_PUBLIC_WEBSOCKET_ID!,
       wsHost: "ws.money.orki.io",
-      forceTLS: !isLocalhost,
+      forceTLS: false,
       enabledTransports: ["ws", "wss"],
       wsPort: 80,
       wssPort: 80,
