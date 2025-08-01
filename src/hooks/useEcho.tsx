b9e052bc991875413c6f5552b9715c863dd75aff
Major-chuks <chukaezeka@gmail.com>
2025-08-01 20:38:54 +0100

    debug: update websocket configuration

--- src/hooks/useEcho.tsx
+++ src/hooks/useEcho.tsx
@@ -19,20 +19,20 @@ export function useEcho() {
       window.Pusher = Pusher;
     }
 
-    const isLocalhost =
-      window.location.hostname === "localhost" ||
-      window.location.hostname === "127.0.0.1" ||
-      window.location.hostname === "[::1]";
+    // const isLocalhost =
+    //   window.location.hostname === "localhost" ||
+    //   window.location.hostname === "127.0.0.1" ||
+    //   window.location.hostname === "[::1]";
 
     // Initialize Echo
     const echoInstance = new Echo({
       broadcaster: "reverb",
       key: process.env.NEXT_PUBLIC_WEBSOCKET_ID!,
       wsHost: "ws.money.orki.io",
-      forceTLS: !isLocalhost,
+      forceTLS: true,
       enabledTransports: ["ws", "wss"],
-      wsPort: 80,
-      wssPort: 80,
+      // wsPort: 80,
+      wssPort: 443,
     });
 
     // Only bind if connector is a PusherConnector and has 'pusher'
