c7ef9ff7b657cf578857bfc945255d762abcdbeb
Major-chuks <chukaezeka@gmail.com>
2025-08-19 11:00:43 +0100

    style: update widget styles for mobile view

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -2,33 +2,23 @@
 
 const Widget = () => {
   return (
-    <div
+    <iframe
+      src="https://orki-widget.vercel.app/"
+      width="100%"
+      height="100%"
+      allow="camera;fullscreen;accelerometer;gyroscope;magnetometer;payment"
+      allowFullScreen
       style={{
-        position: "relative",
         width: "100%",
         maxWidth: "482px",
-        height: "742px",
-        overflow: "auto",
+        height: "748px",
+        background: "#fff",
+        border: "none",
         boxShadow: "0px 2px 50px 0px rgba(0, 0, 0, 0.12)",
         borderRadius: "8px",
       }}
-    >
-      <iframe
-        src="https://orki-widget.vercel.app/"
-        width="100%"
-        height="100%"
-        allow="camera;fullscreen;accelerometer;gyroscope;magnetometer;payment"
-        allowFullScreen
-        style={{
-          border: "none",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          width: "100%",
-        }}
-        title="Orki Money"
-      ></iframe>
-    </div>
+      title="Orki Money"
+    ></iframe>
   );
 };
 
